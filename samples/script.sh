0ed41f7583cc863d5a59d68a1bd3cacacf849749
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-07-27 19:50:38 +1000

    use binary
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- samples/script.sh
+++ samples/script.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Download tracepusher binary
+# Do this ONCE, outside of the shell script!
+# This binary shown below is for MacOS
+# Change tracepusher_binary as appropriate for your environment
+#   windows = tracepusher_${tracepusher_version}.exe
+#   linux = tracepusher_linux_x64_${tracepusher_version}
+#
+# If you can't sudo, remove that line and run tracepusher
+# from the local directory
+# ie. change "tracepusher" to "./tracepusher" on lines 45 and 65
+#
+# == DOWNLOAD CODE - DO THIS ONCE ==
+# tracepusher_version=0.8.0
+# tracepusher_binary=tracepusher_darwin_${tracepusher_version}
+# wget --quiet -O tracepusher https://github.com/agardnerIT/tracepusher/releases/download/${tracepusher_version}/${tracepusher_binary}
+# chmod +x tracepusher
+# sudo mv tracepusher /usr/local/bin
+
 trace_id=$(openssl rand -hex 16)
 span_id=$(openssl rand -hex 8)
 
@@ -23,7 +41,7 @@ do
   duration=$(( $time_end - $time_start ))
   echo "loop time_start: ${time_start}. time_end: ${time_end}. duration: ${duration}"
 
-  python3 tracepusher.py \
+  tracepusher \
     --endpoint=http://localhost:4318 \
     --service-name=serviceA \
     --span-name="subspan${counter}" \
@@ -43,7 +61,7 @@ time_end=$SECONDS
 echo "main time_start: ${main_time_start}. time_end: ${time_end}"
 
 echo "pushing main_trace with duration: ${time_end} and trace_id: ${trace_id} and span_id=${span_id} and time shifted"
-python3 tracepusher.py \
+tracepusher \
   --endpoint=http://localhost:4318 \
   --service-name=serviceA \
   --span-name="main_span" \
